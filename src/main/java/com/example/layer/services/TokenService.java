d0237b77de7034f183450418175db52bf7644508
anishchirakkal <anishchirakkal@gmail.com>
2022-02-14 10:53:58 +0530

    minorModifications

--- src/main/java/com/example/layer/services/TokenService.java
+++ src/main/java/com/example/layer/services/TokenService.java
@@ -38,7 +38,7 @@ public class TokenService {
 		try {
 			json = mapper.writer().writeValueAsString(tokenRequest);
 		} catch (JsonProcessingException e1) {
-			// TODO Auto-generated catch block
+
 			e1.printStackTrace();
 		}
 
